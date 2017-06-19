70eb43779a5299323599250c3426aa979f7f7cef
“gujinjie” <gujinjie@innobuddy.com>
2017-06-19 11:09:56 +0800

    简化接入步骤，支持一键屏蔽

--- app/src/main/java/jaygoo/jacocotest/TestActivity.java
+++ app/src/main/java/jaygoo/jacocotest/TestActivity.java
@@ -12,12 +12,8 @@ import com.karumi.dexter.MultiplePermissionsReport;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
-
 import java.util.List;
-
 import jaygoo.jacocohelper.JacocoHelper;
-
-
 
 
 /**
@@ -29,11 +25,12 @@ import jaygoo.jacocohelper.JacocoHelper;
  * 3、开始测试，测试结束后点击生成报告将ec文件pull到本地项目 PROJECT_PATH 路径中，具体命令可查看日志Loge<Pull>
  * PROJECT_PATH '项目路径/app/build/' + 'outputs/code-coverage/'
  * 4、./gradlew jacocoTestReport 然后到 /app/build/reports里查看相关报告
+ *
  */
 public class TestActivity extends AppCompatActivity {
 
     //请修改为自己的项目路径，后面的文件夹如果没有可以通过./gradlew jacocoInit创建
-    private static String PROJECT_PATH = "/Users/mac/Github/JacocoTestHelper/app/build/outputs/code-coverage/";
+    private static String PROJECT_PATH = "/Users/mac/Github/JacocoTestHelper";
     static final String[] PERMISSIONS = new String[]{
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -44,22 +41,11 @@ public class TestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
         JacocoHelper.init(PROJECT_PATH,true);
-        Dexter.withActivity(this).withPermissions(PERMISSIONS)
-                .withListener(new MultiplePermissionsListener() {
-                    @Override
-                    public void onPermissionsChecked(MultiplePermissionsReport report) {
-                        if (report.areAllPermissionsGranted()){
-                            Toast.makeText(getApplication(),"权限获取成功！",Toast.LENGTH_LONG).show();
-                        }
-                    }
+        initPermissions();
+        initView();
+    }
 
-                    @Override
-                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
-
-                    }
-                }).check();
-
-
+    private void initView() {
         findViewById(R.id.function1_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -88,7 +74,6 @@ public class TestActivity extends AppCompatActivity {
             }
         });
 
-
         findViewById(R.id.report_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -96,6 +81,25 @@ public class TestActivity extends AppCompatActivity {
                 JacocoHelper.generateEcFile(true);
             }
         });
+    }
+
+    /**
+     * for above 6.0 android system
+     */
+    private void initPermissions() {
+        Dexter.withActivity(this).withPermissions(PERMISSIONS)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        if (report.areAllPermissionsGranted()){
+                            Toast.makeText(getApplication(),"权限获取成功！",Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                    }
+                }).check();
     }
 
     private void function1(){
